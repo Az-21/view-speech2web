635334d0dc44d930161311d6d682059439785904
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-10-18 12:12:13 +0530

    Create header component

---
+++ src/lib/component/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let link_color = "text-neutral-100";
+  let hover_link_color = "hover:text-emerald-500";
+</script>
+
+<nav class="bg-neutral-900 p-4 w-full">
+  <div class="mx-auto w-11/12 flex justify-between items-center">
+    <!-- Left: Project Name -->
+    <div class="text-white text-lg font-bold">Speech2Web Database</div>
+    <!-- Right: Links -->
+    <div class="flex space-x-4">
+      <a href="/" class="{link_color} {hover_link_color}">Home</a>
+      <a
+        href="https://github.com/atishnaik8805/speech2web-agents"
+        class="{link_color} {hover_link_color}">GitHub</a
+      >
+    </div>
+  </div>
+</nav>
