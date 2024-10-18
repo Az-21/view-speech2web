5872d507d5d2dd6071ec642d16cebbe5904b973f
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-10-18 12:12:39 +0530

    Add global background and text color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,12 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+  import "../app.css";
+</script>
+
+<slot></slot>
+
+<style lang="postcss">
+  :global(body) {
+    @apply bg-neutral-800;
+    @apply text-white;
+  }
+</style>
