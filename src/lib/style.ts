7367008a9aaa816b591e2da92ce56665b926164c
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-10-18 18:20:27 +0530

    Change header background

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -1,7 +1,7 @@
 export const style = {
   table: {
     self: "table-auto w-full overflow-auto border-2 border-neutral-700",
-    header: "sticky top-0 bg-green-500 py-4 text-left px-4 text-black",
+    header: "sticky top-0 bg-zinc-700 py-4 text-left px-4",
     row: "p-4 even:bg-neutral-800",
     body: "bg-neutral-900 divide-y-2 divide-neutral-700",
   },
