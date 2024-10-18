503574c8a8839b7551211d011885fcd8a77d20ab
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-10-18 12:13:26 +0530

    Add `EventsDB.Jobs` table

---
+++ src/routes/mongodb/EventsDB/Jobs/+page.server.ts
@@ -0,0 +1,46 @@
+import { env } from "$env/dynamic/private";
+import { MongoClient, ObjectId } from "mongodb";
+import type { PageServerLoad } from "./$types";
+
+interface Person {
+  UserID: string;
+  Name: string;
+}
+
+interface Task {
+  Description: string;
+  Date: string;
+  Duration: string;
+  Client: string;
+}
+
+interface Jobs {
+  _id: ObjectId;
+  Person: Person;
+  Task: Task;
+}
+
+export const load: PageServerLoad = async () => {
+  const uri = env.MONGO_URI as string;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("EventsDB");
+    const usersCollection = database.collection<Jobs>("Jobs");
+
+    const jobs = await usersCollection.find({}).toArray();
+
+    return {
+      jobs: jobs.map((job) => ({
+        ...job,
+        _id: job._id.toString(),
+        /// Fallback options
+        Person: job.Person ?? {},
+        Task: job.Task ?? {},
+      })),
+    };
+  } finally {
+    await client.close();
+  }
+};
---
+++ src/routes/mongodb/EventsDB/Jobs/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Header from "$lib/component/Header.svelte";
+  import type { PageData } from "./$types";
+  export let data: PageData;
+
+  const header_css = "sticky top-0 bg-green-900 py-3 text-left px-4";
+</script>
+
+<Header></Header>
+<div class="w-11/12 mx-auto">
+  <div class="prose py-8">
+    <code class="text-white text-lg">EventsDB.Jobs</code>
+  </div>
+
+  <table class="table-auto w-full overflow-auto">
+    <thead>
+      <tr>
+        <th class={header_css}>ID</th>
+        <th class={header_css}>Name</th>
+        <th class={header_css}>Client</th>
+        <th class={header_css}>Description</th>
+        <th class={header_css}>Date</th>
+        <th class={header_css}>Duration</th>
+        <th class={header_css}>User ID</th>
+      </tr>
+    </thead>
+    <tbody class="bg-neutral-700">
+      {#each data.jobs as job}
+        <tr class="odd:bg-neutral-800">
+          <td class="p-4">{job._id}</td>
+          <td class="p-4">{job.Person.Name}</td>
+          <td class="p-4">{job.Task.Client}</td>
+          <td class="p-4">{job.Task.Description}</td>
+          <td class="p-4">{job.Task.Date}</td>
+          <td class="p-4">{job.Task.Duration}</td>
+          <td class="p-4">{job.Person.UserID}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
